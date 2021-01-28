a0a4e2e8c3905a7ecf0e6ed2ae4ad95d16c11e0d
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2021-01-29 00:33:17 +0530

    demo copde added

---
+++ event bubbling demo.js
@@ -0,0 +1,10 @@
+const div1 = document.querySelector("#div1");
+const div2 = document.querySelector("#div2");
+const div3 = document.querySelector("#div3");
+
+// Syntax to catch an event on the bubbling phase
+// div.addEventListener("click", function (event) { alert("Clicked on div 1") } );
+
+div1.addEventListener("click", function (event) { alert("Clicked on div 1") });
+div2.addEventListener("click", function (event) { alert("Clicked on div 2") });
+div3.addEventListener("click", function (event) { alert("Clicked on div 3") });
---
+++ event capturing demo.js
@@ -0,0 +1,14 @@
+const div1 = document.querySelector("#div1");
+const div2 = document.querySelector("#div2");
+const div3 = document.querySelector("#div3");
+
+// Syntax to catch an event on the capturing phase
+// div.addEventListener("click",
+//     function (event) { alert("Clicked on div 1") }, { capture: true }
+// );
+
+// using "true" is an alias to { capture: true }
+
+div1.addEventListener("click", function (event) { alert("Clicked on div 1") }, true);
+div2.addEventListener("click", function (event) { alert("Clicked on div 2") }, true);
+div3.addEventListener("click", function (event) { alert("Clicked on div 3") }, true);
