f836317285db14fc3878d046a791b46da518a1b3
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2021-01-29 00:37:05 +0530

    text fix

--- event bubbling demo.js
+++ event bubbling demo.js
@@ -3,7 +3,7 @@ const div2 = document.querySelector("#div2");
 const div3 = document.querySelector("#div3");
 
 // Syntax to catch an event on the bubbling phase
-// div.addEventListener("click", function (event) { alert("Clicked on div 1") } );
+// div.addEventListener("click", function (event) { alert("Clicked on div") } );
 
 div1.addEventListener("click", function (event) { alert("Clicked on div 1") });
 div2.addEventListener("click", function (event) { alert("Clicked on div 2") });
--- event capturing demo.js
+++ event capturing demo.js
@@ -4,7 +4,7 @@ const div3 = document.querySelector("#div3");
 
 // Syntax to catch an event on the capturing phase
 // div.addEventListener("click",
-//     function (event) { alert("Clicked on div 1") }, { capture: true }
+//     function (event) { alert("Clicked on div") }, { capture: true }
 // );
 
 // using "true" is an alias to { capture: true }
